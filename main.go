d1de14f5d4c637f12f5ef2fe4d7d3a748e0c76bc
zenmetsu <vespira@gmail.com>
2019-10-11 11:46:45 -0700

    Update main.go

--- main.go
+++ main.go
@@ -70,10 +70,9 @@ func CreateGraphiteMetrics(samples model.Vector, metricPrefix string) string {
 	for _, sample := range samples {
 		name := fmt.Sprintf("%s%s", metricPrefix, sample.Metric["__name__"])
 
-		if math.IsNan(Value) {
-			value := 0
-		} else {
-			value := strconv.FormatFloat(float64(sample.Value), 'f', -1, 64)
+		value := 0
+		if !math.IsNan(sample.Value) {
+			value : strconv.FormatFloat(float64(sample.Value), 'f', -1, 64)
 		}
 
 		now := time.Now()
